28774da7a3ebad32e17ad84386f23cf62477539c
Anthony Hadwan <anthony.hadwan@hec.edu>
2021-01-19 18:17:44 +0200

    Added a form to add a new flat with links

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -5,12 +5,14 @@ class FlatsController < ApplicationController
   end
 
   def show
+    @flat = Flat.find_by(params[:id]);
   end
 
   def create
   end
 
   def new
+    @flat = Flat.new
   end
 
   def edit
